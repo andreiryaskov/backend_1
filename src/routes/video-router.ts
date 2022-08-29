572ef20c11a4b0ef7718f0e7b76d0e8a6510863b
andreiryaskov <andreiryaskov@gmail.com>
2022-08-29 17:39:42 +0300

    fix: delete 100544

--- src/routes/video-router.ts
+++ src/routes/video-router.ts
@@ -12,14 +12,50 @@ let videos = [
 
 
 videoRouter.get('/', (req: Request, res: Response) => {
-    return res.status(200).send(videos)
+    res.send(videos)
 })
 
 videoRouter.post('/', (req: Request, res: Response) => {
-    const title = req.body.title
+    let title = req.body.title
+    if (!title || typeof title !== "string" || !title.trim() || title.length > 40) {
+        res.status(400).send({
+            errorsMessages: [
+                {
+                    "message": "string",
+                    "field": "string"
+                }
+            ],
+            resultCode: 1
+        })
+        return
+    }
 
-    if (!title || title.length > 40 || !title.trim() || title !== "string") {
-        return res.status(400).send({
+    const newVideo = {
+        "id": videos.length + 1,
+        "title": "string",
+        "author": "string"
+    }
+    videos.push(newVideo)
+    res.status(201).send(newVideo)
+})
+
+videoRouter.get('/:id', (req: Request, res: Response) => {
+    const id = +req.params.id
+    const video = videos.find(v => v.id !== id)
+    if (video) {
+        res.status(200).send(video)
+        return
+    } else {
+        res.status(404)
+    }
+})
+
+videoRouter.put('/:id', (req: Request, res: Response) => {
+    const id = +req.params.id
+    const video = videos.find(v => v.id === id)
+    const title = req.body.title
+    if (title.length > 40) {
+        res.status(400).send({
             errorsMessages: [
                 {
                     "message": "string",
@@ -27,74 +63,24 @@ videoRouter.post('/', (req: Request, res: Response) => {
                 }
             ]
         })
+        return
     }
-
-    const newVideo = {
-        "id": videos.length + 1,
-        "title": "title",
-        "author": "string"
-    }
-    videos.push(newVideo)
-    res.status(201).send(videos)
-})
-
-videoRouter.get('/:id', (req: Request, res: Response) => {
-    const id = +req.params.id
-    const video = videos.find(v => v.id === id)
-
     if (video) {
-        return res.status(200).send(video)
+        video.title = title
+        res.status(204).send(video)
+        return
     } else {
-        return res.status(404)
-    }
-
-})
-
-videoRouter.put('/:id', (req: Request, res: Response) => {
-    const id = +req.params.id
-    const title = req.body.title
-    const updateVideo = videos.find(v => v.id === id)
-
-    if (title.length > 40) {
-        return res.status(400).send({
-            "errorsMessages": [
-                {
-                    "message": "title is incorrect values",
-                    "field": "string"
-                }
-            ]
-        })
-    }
-
-
-    if (updateVideo) {
-
-        updateVideo.title = title
-        return res.status(204).send(updateVideo)
-    } else {
-        return res.status(404)
+        res.send(404)
     }
 })
 
-//Delete video id
 videoRouter.delete('/:id', (req: Request, res: Response) => {
     const id = +req.params.id
-    const video = videos.find(v => v.id === id)
-
-    // if (videos.length === newVideosArray.length) {
-    //
-    // }
-    if (video) {
-        for (let i = 0; i < videos.length; i++) {
-            if (videos[i].id === id) {
-                videos.splice(i, 1)
-                res.send(204)
-                return
-            }
-        }
+    const newVideo = videos.filter(v => v.id !== id)
+    if(newVideo.length < videos.length) {
+        videos = newVideo
+        res.send(204)
     } else {
-        return res.status(404)
+        res.send(404)
     }
-
-
 })
