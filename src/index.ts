42e9fb142b39535e50df927133cfce219b02f1a4
andreiryaskov <andreiryaskov@gmail.com>
2022-09-13 17:47:41 +0300

    fix: express-validation off auth-middleware

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,17 @@ import bodyParser from "body-parser";
 import {videoRouter} from "./routes/video-router";
 import {testingRouter} from "./routes/testing-router";
 import {blogsRouter} from "./routes/blogs-router";
-import {authMiddleware} from "./middlewares/auth-middleware";
 
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
-app.use(authMiddleware)
+// app.use(authMiddleware)
 app.use(bodyParser())
 app.use(cors())
 app.use('/videos', videoRouter)
 app.use('/testing', testingRouter)
 app.use('/blogs', blogsRouter)
-
-
-
-
 
 app.listen(port, () => {
     console.log('========server ok=============')
--- src/repositories/video-repositories.ts
+++ src/repositories/video-repositories.ts
@@ -80,6 +80,6 @@ export const videosRepositories = {
     }
     ,
     deleteAllData() {
-        return videos = []
+        return []
     }
 }
