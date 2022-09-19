d60d2f86f67c47a6806fe2c8a72c52e89125e974
andreiryaskov <andreiryaskov@gmail.com>
2022-09-19 19:39:14 +0300

    onlyFirstError true

--- src/middlewares/validationMiddleware.ts
+++ src/middlewares/validationMiddleware.ts
@@ -4,7 +4,7 @@ import {validationResult} from "express-validator";
 export const validationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const errorsMessages = validationResult(req)
     if (!errorsMessages.isEmpty()) {
-        let newErrors = errorsMessages.array();
+        let newErrors = errorsMessages.array({onlyFirstError: true});
         res.status(400).json({
             errorsMessages: newErrors.map((e) => ({
                 message: e.msg,
